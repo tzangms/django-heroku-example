7c04b053ddfc0ce81ef30060f4c8398b84f69a3e
tzangms <tzangms@gmail.com>
2011-12-10 07:22:43 -0800

    setup urls.py for serve staticfiles on DEBUG

--- herokutest/urls.py
+++ herokutest/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,3 +11,6 @@ urlpatterns = patterns('',
     (r'^$', 'app.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
